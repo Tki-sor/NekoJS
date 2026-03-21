fa9b7c0694b1a24f4e1c39a2cb99cdc9acf7b6c9
ZZZank <3410764033@qq.com>
2026-03-21 11:19:27 +0800

    use original ServerTickEvent, connected to neoforge bus via EventBusForgeBridge

--- src/main/java/com/tkisor/nekojs/bindings/event/ServerEvents.java
+++ src/main/java/com/tkisor/nekojs/bindings/event/ServerEvents.java
@@ -1,18 +1,23 @@
 package com.tkisor.nekojs.bindings.event;
 
+import com.tkisor.nekojs.api.event.EventBusForgeBridge;
 import com.tkisor.nekojs.api.event.EventBusJS;
 import com.tkisor.nekojs.api.event.EventGroup;
 import com.tkisor.nekojs.wrapper.event.server.RecipeEventJS;
-import com.tkisor.nekojs.wrapper.event.server.ServerTickEventJS;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
 
 public interface ServerEvents {
     EventGroup GROUP = EventGroup.of("ServerEvents");
 
-    EventBusJS<ServerTickEventJS, Void> TICK_PRE =
-            GROUP.server("tickPre", ServerTickEventJS.class);
-
-    EventBusJS<ServerTickEventJS, Void> TICK_POST =
-            GROUP.server("tickPost", ServerTickEventJS.class);
+    EventBusJS<ServerTickEvent.Pre, Void> TICK_PRE =
+            GROUP.server("tickPre", ServerTickEvent.Pre.class);
+    EventBusJS<ServerTickEvent.Post, Void> TICK_POST =
+            GROUP.server("tickPost", ServerTickEvent.Post.class);
 
     EventBusJS<RecipeEventJS, Void> RECIPES = GROUP.server("recipes", RecipeEventJS.class);
+
+    EventBusForgeBridge FORGE_BRIDGE = EventBusForgeBridge.create(NeoForge.EVENT_BUS)
+        .bind(TICK_PRE)
+        .bind(TICK_POST);
 }
