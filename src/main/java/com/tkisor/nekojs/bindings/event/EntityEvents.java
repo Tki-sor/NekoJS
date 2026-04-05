e9e5195db2ea3c852bc37b97a997fe826a9d7c2c
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-04-05 22:46:46 +0800

    get rid of event wrapper in `EntityEvents`, all dispatched via `EntityType`

--- src/main/java/com/tkisor/nekojs/bindings/event/EntityEvents.java
+++ src/main/java/com/tkisor/nekojs/bindings/event/EntityEvents.java
@@ -4,31 +4,33 @@ import com.tkisor.nekojs.api.event.EventBusForgeBridge;
 import com.tkisor.nekojs.api.event.EventBusJS;
 import com.tkisor.nekojs.api.event.EventGroup;
 import com.tkisor.nekojs.utils.event.dispatch.DispatchKey;
-import com.tkisor.nekojs.wrapper.event.entity.EntityDeathEventJS;
-import com.tkisor.nekojs.wrapper.event.entity.EntityHurtPostEventJS;
-import com.tkisor.nekojs.wrapper.event.entity.EntityHurtPreEventJS;
-import com.tkisor.nekojs.wrapper.event.entity.EntitySpawnedEventJS;
-import com.tkisor.nekojs.wrapper.event.player.PlayerChatEventJS;
+import net.minecraft.world.entity.EntityType;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.ServerChatEvent;
+import net.neoforged.neoforge.event.entity.EntityEvent;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+
+import java.util.function.Function;
 
 public interface EntityEvents {
     EventGroup GROUP = EventGroup.of("EntityEvents");
 
-    EventBusJS<EntityHurtPreEventJS, String> HURT_PRE =
-            GROUP.server("hurtPre", EntityHurtPreEventJS.class, DispatchKey.string());
+    Function<EntityEvent, EntityType<?>> TO_KEY = e -> e.getEntity().getType();
 
-    EventBusJS<EntityHurtPostEventJS, String> HURT_POST =
-            GROUP.server("hurtPost", EntityHurtPostEventJS.class, DispatchKey.string());
+    EventBusJS<LivingDamageEvent.Pre, EntityType<?>> HURT_PRE =
+            GROUP.server("hurtPre", LivingDamageEvent.Pre.class, DispatchKey.of(EntityType.class, TO_KEY));
+    EventBusJS<LivingDamageEvent.Post, EntityType<?>> HURT_POST =
+            GROUP.server("hurtPost", LivingDamageEvent.Post.class, DispatchKey.of(EntityType.class, TO_KEY));
 
-    EventBusJS<EntityDeathEventJS, String> DEATH =
-            GROUP.server("death", EntityDeathEventJS.class, DispatchKey.string());
-    EventBusJS<EntitySpawnedEventJS, Void> SPAWNED =
-            GROUP.server("spawned", EntitySpawnedEventJS.class);
+    EventBusJS<LivingDeathEvent, EntityType<?>> DEATH =
+            GROUP.server("death", LivingDeathEvent.class, DispatchKey.of(EntityType.class, TO_KEY));
+    EventBusJS<EntityJoinLevelEvent, EntityType<?>> SPAWNED =
+            GROUP.server("spawned", EntityJoinLevelEvent.class, DispatchKey.of(EntityType.class, TO_KEY));
 
     EventBusForgeBridge FORGE_BRIDGE = EventBusForgeBridge.create(NeoForge.EVENT_BUS)
-            .bindTransformed(DEATH, EntityDeathEventJS::new, LivingDeathEvent.class)
-            .bindTransformed(SPAWNED, EntitySpawnedEventJS::new, EntityJoinLevelEvent.class);
+        .bind(HURT_PRE)
+        .bind(HURT_POST)
+        .bind(DEATH)
+        .bind(SPAWNED);
 }
